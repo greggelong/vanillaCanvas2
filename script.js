415200e617ab2c484788c7de5e3ac9debbb85564
Greg K <gkreisman_cep@qq.com>
2022-04-02 22:18:16 +0800

    only button listener

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ window.addEventListener('load', function(){
     //set width and height
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-       
+
+    // button control
+    const mybutton = this.document.getElementById('mybutton');
+    mybutton.addEventListener('click', randomit)
+    
     // with lines
     ctx.strokeStyle='yellow';
     ctx.lineWidth = 10;
@@ -20,21 +24,54 @@ window.addEventListener('load', function(){
     ctx.shadowOffsetX=10;
     ctx.shadowOffsetY =5;
     ctx.shadowBlur =10;
-    let sides, size, maxLevel, branches, scl;
+    let size, maxLevel, branches, scl;
 
+    
+    size = 200;
+    maxLevel =5;
+        //spread = 0.32;
+        //console.log("boo"+spread)
+    branches = 2;
+    scl = 0.8; //scale
+    spread = (33 * Math.PI/180)
+    clearAndCall();
+    
+
+    function randomit(){
+      // number of branches
+      branches = Math.floor((Math.random()*3)+1)
+      let ang =Math.floor((Math.random()* (90-11)+10))
+      spread = ang * Math.PI/180
+      scl = Math.random() * (0.8-0.3) +0.3; 
+      //Math.floor(Math.random() * (max - min + 1) + min) random between two numbers
+      maxLevel = Math.floor(Math.random()*(4))+2
+      console.log(scl);
+      // color
+      let r = Math.floor(Math.random()*255);
+        let g = Math.floor(Math.random()*255);
+        let b = Math.floor(Math.random()*255);
+        //console.log(r,g,b)
+        ctx.strokeStyle = `rgb(${r},${g},${b})`; // variables inside back ticks
+        // add clear and call when not using the mouse
+        clearAndCall();
+
+
+    }
+/*
     canvas.addEventListener('mousemove', e => {
         //console.log(e.offsetX)
         spread = (e.offsetX)*0.001;
         //console.log(spread)
         // need to call the function
+        
+        clearAndCall();
+
+    
+    })
+*/
+    function clearAndCall(){
         ctx.clearRect(0,0,canvas.width,canvas.height);
-        sides = 5;
-        size = 200;
-        maxLevel =5;
-        //spread = 0.32;
-        //console.log("boo"+spread)
-        branches = 2;
-        scl = 0.8;
+        
         ctx.save(); 
         ctx.translate(canvas.width/2,canvas.height-30);
         ctx.scale(1,1);
@@ -43,8 +80,10 @@ window.addEventListener('load', function(){
         drawBranch(0);
         ctx.restore();
 
-    
-    })
+
+
+
+    }
 
 
     
